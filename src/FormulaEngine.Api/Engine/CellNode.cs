d5709de9419de8e0f9f8825cd90f0ab902590e58
Gabrielle Kurah <gskurah@yahoo.co.uk>
2026-05-04 17:26:08 +0200

    feat: add CellNode and DependencyGraph to engine

---
+++ src/FormulaEngine.Api/Engine/CellNode.cs
@@ -0,0 +1,23 @@
+﻿namespace FormulaEngine.Api.Engine;
+
+public class CellNode
+{
+    public Guid Id { get; init; }
+    
+    public Guid WorkspaceId { get; init; }
+
+    public string Key { get; init; } = null!;
+    
+    public decimal Value { get; set; }
+    
+    public string? Expression { get; init; }
+    
+    public CellNode(Guid id, Guid workspaceId, string key, decimal value, string? expression)
+    {
+        Id = id;
+        WorkspaceId = workspaceId;
+        Key = key;
+        Value = value;
+        Expression = expression;
+    }
+}
---
+++ src/FormulaEngine.Api/Engine/DependencyGraph.cs
@@ -0,0 +1,31 @@
+﻿namespace FormulaEngine.Api.Engine;
+
+public class DependencyGraph
+{
+    private readonly Dictionary<Guid, CellNode> _nodes = new();
+    private readonly Dictionary<Guid, HashSet<Guid>> _outgoingEdges = new();
+    private readonly Dictionary<Guid, HashSet<Guid>> _incomingEdges = new();
+
+    public void AddNode(CellNode node)
+    {
+        _nodes[node.Id] = node;
+        _outgoingEdges.TryAdd(node.Id,[]);
+        _incomingEdges.TryAdd(node.Id, []);
+    }
+
+    public void AddEdge(Guid fromCellId, Guid toCellId)
+    {
+        _outgoingEdges[fromCellId].Add(toCellId);
+        _incomingEdges[toCellId].Add(fromCellId);
+    }
+
+    public CellNode? GetNode(Guid id) => _nodes.GetValueOrDefault(id);
+
+    public IReadOnlySet<Guid> GetOutgoing(Guid id) =>
+        _outgoingEdges.GetValueOrDefault(id) ?? [];
+
+    public IReadOnlySet<Guid> GetIncoming(Guid id) =>
+        _incomingEdges.GetValueOrDefault(id) ?? [];
+
+    public IEnumerable<CellNode> GetAllNodes() => _nodes.Values;
+}
