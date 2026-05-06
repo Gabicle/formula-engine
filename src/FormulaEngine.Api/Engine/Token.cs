77179587d987c02edeeaaf378c2e33ab7be6f61f
Gabrielle Kurah <gskurah@yahoo.co.uk>
2026-05-06 20:35:21 +0200

    feat: add token and tokentypes

---
+++ src/FormulaEngine.Api/Engine/Token.cs
@@ -0,0 +1,15 @@
+﻿namespace FormulaEngine.Api.Engine;
+
+public record Token(TokenType Type, string Value)
+{
+    public static readonly Token Plus              = new(TokenType.Plus,             "+");
+    public static readonly Token Minus             = new(TokenType.Minus,            "-");
+    public static readonly Token Multiply          = new(TokenType.Multiply,         "*");
+    public static readonly Token Divide            = new(TokenType.Divide,           "/");
+    public static readonly Token GreaterThan       = new(TokenType.GreaterThan,      ">");
+    public static readonly Token LessThan          = new(TokenType.LessThan,         "<");
+    public static readonly Token EqualSign = new(TokenType.Equals, "=");    public static readonly Token LeftParenthesis   = new(TokenType.LeftParenthesis,  "(");
+    public static readonly Token RightParenthesis  = new(TokenType.RightParenthesis, ")");
+    public static readonly Token Comma             = new(TokenType.Comma,            ",");
+    public static readonly Token EndOfExpression   = new(TokenType.EndOfExpression,  "");
+}
---
+++ src/FormulaEngine.Api/Engine/TokenType.cs
@@ -0,0 +1,19 @@
+﻿namespace FormulaEngine.Api.Engine;
+
+public enum TokenType
+{
+    Number,
+    CellReference,
+    Function,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    GreaterThan,
+    LessThan,
+    Equals,
+    LeftParenthesis,
+    RightParenthesis,
+    Comma,
+    EndOfExpression
+}
